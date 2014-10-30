a3caa54948b3cb55c4a63ab3de702c0a9d379e16
Denis Jacquemin <denis.jacquemin@gmail.com>
2014-10-30 09:02:12 +0100

    moving statics files to static folder

--- search-ui-server.js
+++ search-ui-server.js
@@ -2,11 +2,31 @@ var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/static'))
 
-app.get('/', function(request, response) {
-    response.send('Hello World!')
+app.get('/:filename', function(req, res) {
+    var fileName = req.params.filename;
+
+    var options = {
+        root: __dirname + '/static/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else {
+            console.log('Sent:', fileName);
+        }
+    });
 })
+
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))

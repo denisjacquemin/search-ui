6b69130afc63aa53a30e46b0e24ae2a024ecf9d5
Denis Jacquemin <denis.jacquemin@gmail.com>
2014-10-31 09:37:10 +0100

    adding ejs engine

--- search-ui-server.js
+++ search-ui-server.js
@@ -1,8 +1,12 @@
-var express = require('express')
+var express = require('express');
+//    device  = require('device');
 var app = express();
 
+
 app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static(__dirname + '/static'))
+//app.use(device.capture());
 
 app.get('/:filename', function(req, res) {
     var fileName = req.params.filename;
@@ -26,7 +30,6 @@ app.get('/:filename', function(req, res) {
         }
     });
 })
-
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
